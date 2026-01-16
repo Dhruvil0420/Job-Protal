324fc19b084d3037fa5a3b634f53dff94fc9d4d8
Dhruvil0420 <dhruvilparmar1819@gmail.in>
2026-01-16 18:49:23 +0530

    fix: remove console log from webhook error handling for cleaner output

--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -56,7 +56,6 @@ export const clerkWebhooks = async (req, res) => {
                 break;
         }
     } catch (error) {
-        console.log(error.message);
         res.json({success:false,message: 'Webhooks Error'})
     }
 }
