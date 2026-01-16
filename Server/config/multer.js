57f1d2381f8f391f87900769e86b1978b69e8254
Dhruvil0420 <dhruvilparmar1819@gmail.in>
2026-01-16 21:47:54 +0530

    fix: switch multer storage from disk to memory for improved file handling

--- Server/config/multer.js
+++ Server/config/multer.js
@@ -1,13 +1,7 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
+const upload = multer({
+  storage: multer.memoryStorage(),
 });
-
-
-const upload = multer(storage)
 
 export default upload;
